5f39684cf09c988240b123ebe6225ebf87841ba5
ArunRaj2312 <arunvijay1043534@gmail.com>
2022-11-23 20:41:53 -1200

    add file

---
+++ carousel/main.js
@@ -0,0 +1,30 @@
+$('.loop').owlCarousel({
+    center:true,
+    items:1,
+    loop:true,
+    margin:20,
+	autoplay:true,
+    responsive:{
+		0:{
+			items:1
+		},
+        600:{
+            items:3
+        },
+		1000:{
+			items:5
+		}
+    }
+});
+$('.brand').owlCarousel({
+    center:true,
+    items:2,
+    loop:true,
+    margin:30,
+	autoplay:true,
+    responsive:{
+        600:{
+            items:3
+        }
+    }
+});
